aa68cf804eeadd5ecee0c0eed549525537f49460
Scott Francis <scottfrancis@Scotts-MacBook-Air.local>
2017-01-24 15:59:47 -0800

    setup Vagrant for dev

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Install system requirements
+
+
+sudo apt-get update
+# sudo apt-get upgrade -y
+
+sudo apt-get install -y nodejs
+sudo apt-get install -y ruby ruby-dev
+sudo apt-get install -y sqlite3 libsqlite3-dev
+sudo gem update
+
+sudo gem install bundler
+sudo gem install nokogiri
+
+sudo gem install rails
+
+sudo gem install sqlite3
