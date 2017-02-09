e04bc52cc70993dd95897b5a21394b481571dcac
scott francis <scott.russell.francis@gmail.com>
2017-02-09 10:21:53 -0500

    workin on 13?

--- wk2/todolists/app/models/profile.rb
+++ wk2/todolists/app/models/profile.rb
@@ -1,3 +1,20 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  
+  validates :gender, :inclusion => { :in => ["male","female"] }
+  
+  validate :both_names_not_null, :boy_not_named_sue
+
+ def both_names_not_null
+    if first_name.blank? and last_name.blank?
+      errors.add(:first_name, "first_name and last_name cannot both be empty")
+      errors.add(:last_name, "first_name and last_name cannot both be empty")
+    end
+  end
+  
+  def boy_not_named_sue
+    if !first_name.blank? and first_name == 'Sue' and gender == 'male'
+      errors.add(:gender, "what did you do? a boy named sue!")
+    end
+  end
 end
--- wk2/todolists/app/models/user.rb
+++ wk2/todolists/app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-  has_one :profile
-  has_many :todo_lists
+  has_one :profile, dependent: :destroy
+  has_many :todo_lists, dependent: :destroy #, on_delete: :cascade
   has_many :todo_items, through: :todo_lists, source: :todo_items
+  
+  validates :username, presence: true
+  
+  def get_completed_count
+    todo_items.find({completed: true}).count
+  end
 end
